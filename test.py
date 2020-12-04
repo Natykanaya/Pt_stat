934cc4d52e22ab4f9f1561af83df9d9a51b60a5f
Nika <veronikanatykanaya@gmail.com>
2020-12-04 13:03:39 +0200

    check out

--- test.py
+++ test.py
@@ -18,9 +18,9 @@ def fcbook():
 
 def inst():
     r = requests.get("https://www.instagram.com/progresstech.ua/?hl=ru")
-    print(r)
     soup = bs(r.text, "html.parser")
     podpis = soup.find('meta',property="og:description")
+    print(podpis)
     podp=podpis["content"]
     podpp=podp.split()
     ret = podpp[0]
